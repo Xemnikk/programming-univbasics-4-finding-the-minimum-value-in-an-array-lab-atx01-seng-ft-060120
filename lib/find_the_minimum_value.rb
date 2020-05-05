f34c8345b48e9f62e518b58ded409154633f4ec9
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-05 15:31:54 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,12 @@
 def find_min_value(array)
   # Add your solution here
+  count = 0 
+  min = 9999
+  while count < array.length do 
+    if min > array[count]
+      min = array[count]
+    end 
+    count += 1 
+  end 
+  min
 end
